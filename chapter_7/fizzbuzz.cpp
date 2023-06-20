d30d4f19d9f8323b6185f462b5a5e9625a4fb472
Frieso Turkstra <friesoturkstra@gmail.com>
2023-06-20 10:36:45 +0200

    fizzbuzz program

---
+++ chapter_7/fizzbuzz.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+void fizzbuzz(int value)
+{   
+    for (int i{ 1 }; i <= value; ++i)
+    {   
+        bool printed{ false };
+        if (i % 3 == 0)
+            std::cout << "fizz";
+            printed = true;
+        if (i % 5 == 0)
+            std::cout << "buzz";
+            printed = true;
+        if (i % 7 == 0)
+            std::cout << "pop";
+            printed = true;
+        if (!printed)
+            std::cout << i;
+
+        std::cout << '\n';
+    }
+}
+
+int main()
+{   
+    fizzbuzz(150);
+    return 0;
+}
